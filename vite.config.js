70ce8c153a8aa1335c8604e5a057c788998567fd
TheBrambam <anthony.brambilla@hotmail.com>
2025-12-09 21:16:59 -0500

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+// import { defineConfig } from 'vite'
+// import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [svelte()],
+// })
+
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
 export default defineConfig({
   plugins: [svelte()],
-})
+  base: '/proj4/',
+  build: {
+    outDir: 'docs'
+  }
+});
